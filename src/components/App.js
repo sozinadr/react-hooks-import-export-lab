0dae2d906572ac4d45b6fd30c18d8e53ff99ecd9
sozi <sozyarnadir@gmail.com>
2022-10-20 10:16:19 +0300

    assignment completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import About from "./About.js";
+import Home from "./Home.js";
+import NavBar from "./NavBar.js";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+    return ( <
+        div >
+        <
+        NavBar / >
+        <
+        Home / >
+        <
+        About / >
+        <
+        /div>
+    );
 }
-
 export default App;
